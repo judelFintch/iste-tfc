4a120fb4f1354353d735bdf7fb21582b01c45105
Judel Fintch <judfintch@gmail.com>
2024-08-16 13:17:39 +0200

    feat :adding bootstrap assets

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,100 +1,86 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
+<div class="header">
+    <div class="header-left">
+        <a href="{{route('dashboard')}}" class="logo">
+            <img src="{{asset('assets/img/logo.png')}}" width="40" height="40" alt="">
+        </a>
+    </div>
+    <a id="toggle_btn" href="javascript:void(0);">
+        <span class="bar-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+    </a>
+    <div class="page-title-box">
+        <h3>{{ config('app.name', 'New Castoms') }}</h3>
     </div>
 
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
+    <a id="mobile_btn" class="mobile_btn" href="#sidebar"><i class="fa fa-bars"></i></a>
 
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
+    <ul class="nav user-menu">
 
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
+        <li class="nav-item">
+            <div class="top-nav-search">
+                <a href="javascript:void(0);" class="responsive-search">
+                    <i class="fa fa-search"></i>
+                </a>
+                <form action="search">
+                    <input wire:model.live="coonect" class="form-control" type="text" placeholder="Search here">
+                    <button class="btn" type="submit"><i class="fa fa-search"></i></button>
                 </form>
             </div>
-        </div>
-    </div>
-</nav>
+        </li>
+
+        <li class="nav-item dropdown has-arrow flag-nav">
+            <div class="dropdown-menu dropdown-menu-right">
+                <a href="javascript:void(0);" class="dropdown-item">
+                    <img src="{{asset('assets/img/flags/fr.png')}}" alt="" height="16"> French
+                </a>
+            </div>
+        </li>
+
+        <li class="nav-item dropdown">
+            <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
+                <i class="fa fa-comment-o"></i> <span class="badge rounded-pill">8</span>
+            </a>
+            <div class="dropdown-menu notifications">
+                <div class="topnav-dropdown-header">
+                    <span class="notification-title">Messages</span>
+                    <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+                </div>
+                <div class="noti-content">
+                    <ul class="notification-list">
+                    </ul>
+                </div>
+                <div class="topnav-dropdown-footer">
+                    <a href="apps-chat">View all Messages</a>
+                </div>
+            </div>
+        </li>
+       
+
+        <li class="nav-item dropdown has-arrow main-drop">
+            <a href="#">
+                <span > </span>
+            </a>
+        </li>
+
+        <li class="nav-item dropdown has-arrow main-drop">
+            <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
+                <span class="user-img"><img src="assets/img/profiles/avatar-21.jpg" alt="">
+                    <span class="status online"></span></span>
+                <span> {{ Auth::user()->name }}</span>
+            </a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="">Profile</a>
+               
+                <form method="POST" action="{{ route('logout') }}" x-data>
+                    @csrf
+                    <a class="dropdown-item" href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                        {{ __('Log Out') }}</a>
+                </form>
+            </div>
+        </li>
+    </ul>
+
+</div>
--- resources/views/livewire/students/students.blade.php
+++ resources/views/livewire/students/students.blade.php
@@ -3,22 +3,19 @@
     <div class="page-wrapper">
         <div class="content container-fluid">
             <div class="page-header">
-
-            
                 <div class="row align-items-center">
                     <div class="col">
-                        <h3 class="page-title">Informations Dossiers</h3>
+                        <h3 class="page-title">Inscription</h3>
                         <ul class="breadcrumb">
                             <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
                         </ul>
                     </div>
                     <div class="col-auto float-end ms-auto">
-                        <a href="#" wire:click="showForm()" class="btn add-btn" id="add_client"><i class="fa fa-plus"></i> Nouveau Dossier</a>
+                        <a href="#" wire:click="showForm()" class="btn add-btn" id="add_client"><i class="fa fa-plus"></i> Nouvel Etudiant</a>
                     </div>
                 </div>
             </div>
 
-        
             <div class="row">
                 <div class="col-md-4 col-sm-4 col-lg-4 col-xl-4">
                     <a href="">
@@ -26,7 +23,7 @@
                             <div class="card-body">
                                 <span class="dash-widget-icon"><i class="fa fa-dollar"></i></span>
                                 <div class="dash-widget-info">
-                                <h3></h3>
+                                    <h3></h3>
                                     <span> </span>
                                 </div>
                             </div>
@@ -34,14 +31,13 @@
                     </a>
                 </div>
 
-                
                 <div class="col-md-4 col-sm-4 col-lg-4 col-xl-4">
                     <a href="">
                         <div class="card dash-widget">
                             <div class="card-body">
                                 <span class="dash-widget-icon"><i class="fa fa-money"></i></span>
                                 <div class="dash-widget-info">
-                                <h3></h3>
+                                    <h3></h3>
                                     <span> </span>
                                 </div>
                             </div>
@@ -63,11 +59,7 @@
                     </a>
                 </div>
             </div>
-
-           
-
             <hr>
-
             <div class="row">
                 <legend>Filtrez le Dossier</legend>
                 <div class="col-sm-5">
@@ -75,10 +67,6 @@
                 </div>
             </div>
             <hr>
-
-          
-
-           
         </div>
     </div>
 </div>
