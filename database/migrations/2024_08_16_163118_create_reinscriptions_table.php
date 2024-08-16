c8f3aa930894d35f6b1ee9288366fd6bd0db8017
Judel Fintch <judfintch@gmail.com>
2024-08-16 18:32:42 +0200

    feat :addign migrations

--- database/migrations/2024_08_16_163118_create_reinscriptions_table.php
+++ database/migrations/2024_08_16_163118_create_reinscriptions_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('reinscriptions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('etudiant_id')->constrained()->onDelete('cascade');
+            $table->boolean('valide')->default(false);
             $table->timestamps();
         });
     }
